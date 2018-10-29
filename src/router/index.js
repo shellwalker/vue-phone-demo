bd9a441fbf64dd0915f7c38554441ca1dcbbe714
shellwalker <sagittarius1212@gmail.com>
2018-10-29 10:28:48 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  next() // default logic
+})
+
+export default router
